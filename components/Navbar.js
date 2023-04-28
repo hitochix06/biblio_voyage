660018e8399799b655ab367a3ba60adde92340bf
hitochix06 <pachara.philibert@hotmail.fr>
2023-04-28 10:53:49 +0200

    rejoute navab

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,23 +2,33 @@ import styles from '../styles/Navbar.module.scss';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-bottom position-fixed w-100 z-3">
-            <div className="container">
-                <a className="navbar-brand" href="#">Philibert</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Bibliothèque voyage</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
-                    <div className="navbar-nav ms-auto">
-                        <a className="nav-link ps-4" href="#about">A propos</a>
-                        <a className="nav-link ps-4" href="#skills">Compétences</a>
-                        <a className="nav-link ps-4" href="#experiences">Parcours</a>
-                        <a className="nav-link ps-4" href="#works">Projets</a>
-                        <a className="nav-link ps-4" href="#contact">Contact</a>
-                    </div>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Accueil</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">À propos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Se connecter</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
+
     );
 }
 
